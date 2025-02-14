d1e7681adbb9ff9edc5b5503d5dcd5c298e8de54
Vaishnavi <vaishnavip276@gmail.com>
2025-02-14 22:43:08 +0530

    commit

--- src/components/ThreeScectionPage.tsx
+++ src/components/ThreeScectionPage.tsx
@@ -10,25 +10,25 @@ const ThreeSectionPage = () => {
   const [showSpline, setShowSpline] = useState(false);
 
   useEffect(() => {
-    setShowSpline(true); // Load Spline only on the client
+    setShowSpline(true); 
   }, []);
 
   return (
     <main className="w-full h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#100c08]">
-      {/* Section 1 with Spline */}
+    
       <section className="h-screen relative snap-proximity">
-        {/* Place Header above Spline */}
+    
         <div className="relative z-20">
           <Header />
         </div>
 
-        {/* Overlay Text Behind Spline */}
+        
         <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-[5]">
           <motion.h1
-            className="bg-gradient-to-b from-slate-200 to-slate-400 bg-clip-text text-transparent text-7xl md:text-6xl ultra-bold lg:text-9xl font-[900] text-center [text-shadow:_1px_1px_0_rgb(0_0_0_/_20%)]"
-            initial={{ y: -100, opacity: 0 }} // Start off-screen at the top and invisible
-            animate={{ y: 0, opacity: 1 }} // End in the middle and fully visible
-            transition={{ duration: 1, ease: "easeOut" }} // Animation duration and easing
+            className="bg-gradient-to-b from-slate-200 to-slate-800 bg-clip-text text-transparent text-7xl md:text-6xl ultra-bold lg:text-9xl font-[900] text-center [text-shadow:_1px_1px_0_rgb(0_0_0_/_20%)]"
+            initial={{ y: -100, opacity: 0 }} 
+            animate={{ y: 0, opacity: 1 }} 
+            transition={{ duration: 1, ease: "easeOut" }}
           >
             ZERO GRAVITY
           </motion.h1>
@@ -44,7 +44,7 @@ const ThreeSectionPage = () => {
       </section>
 
       {/* Section 2 */}
-      <section className="h-[150vh] flex flex-col justify-center items-center text-white bg-[#100c08]">
+      <section className="h-[150vh]  text-white bg-[#100c08]">
         <MyMarquee />
       </section>
 
