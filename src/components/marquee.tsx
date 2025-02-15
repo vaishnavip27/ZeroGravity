417e134070d1670312efa6dbe4dc2e77e9a5eca7
Vaishnavi <vaishnavip276@gmail.com>
2025-02-15 21:59:10 +0530

    commit

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -3,7 +3,7 @@ import { HiMiniArrowLongRight } from "react-icons/hi2";
 
 const MyMarquee = () => {
   return (
-    <div className="transform rotate-[-3deg] mt-28 text-slate-400">
+    <div className="transform rotate-[-3deg] mt-44 text-slate-400">
       <Marquee gradient={false}>
         <p className="text-3xl font-normal mx-4">Welcome to Zero Gravity vibes</p>
         <HiMiniArrowLongRight size={54} />
