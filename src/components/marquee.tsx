15f0c390e3826e4867cd84f12a965a85098b1b43
Vaishnavi <vaishnavip276@gmail.com>
2025-02-16 06:13:41 +0530

    commit

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -5,13 +5,17 @@ const MyMarquee = () => {
   return (
     <div className="transform rotate-[-3deg] mt-44 text-slate-400">
       <Marquee gradient={false}>
-        <p className="text-3xl font-normal mx-4">Welcome to Zero Gravity vibes</p>
+        <p className="text-xl font-normal mx-5">Welcome to Zero Gravity vibes</p>
         <HiMiniArrowLongRight size={54} />
-        <p className="text-3xl font-normal mx-4">Ready to Drift?</p>
+        <p className="text-xxl font-normal mx-5">Zero Gravity</p>
         <HiMiniArrowLongRight size={54} />
-        <p className="text-3xl font-normal mx-4">Floating beyond Limits</p>
+        <p className="text-xl font-normal mx-5">Ready to Drift?</p>
         <HiMiniArrowLongRight size={54} />
-        <p className="text-3xl font-normal mx-4">Zero Gravity</p>
+        <p className="text-xxl font-normal mx-5">Zero Gravity</p>
+        <HiMiniArrowLongRight size={54} />
+        <p className="text-xl font-normal mx-5">Floating beyond Limits</p>
+        <HiMiniArrowLongRight size={54} />
+        <p className="text-xxl font-normal mx-5">Zero Gravity</p>
         <HiMiniArrowLongRight size={54} />
       </Marquee>
     </div>
--- src/components/seed.tsx
+++ src/components/seed.tsx
@@ -4,7 +4,7 @@ export interface FaqItem {
 }
 
 export const faqs: FaqItem[] = [
-    { question: "Analyzing Research", answer: "We are collaborating with universities to ensure rigorous validation of information." },
-    { question: "Integrated Learning", answer: "Our platform combines various educational resources to enhance the learning experience and foster deeper understanding." },
-    { question: "Considering Innovation", answer: "We continuously explore new technologies and methodologies to improve our verification processes and user engagement." },
+    { question: "Beyond Earth's pull", answer: "Zero gravity creates true weightlessness, a state where objects and humans float freely. This phenomenon occurs in space stations and specialized aircraft during parabolic flights, where the pull of gravity is perfectly counteracted by constant free fall" },
+    { question: "Body in Space", answer: "The human body undergoes remarkable changes in zero gravity. Astronauts experience temporary height increase as their spine elongates, muscle and bone mass begin to decrease, and bodily fluids shift upward. These changes require special exercise routines and medical monitoring.." },
+    { question: "Future Horizons", answer: "Zero gravity environments enable groundbreaking research in medicine, materials, and space technology. Scientists conduct experiments impossible on Earth, leading to innovations in manufacturing, medicine, and preparing humanity for long-duration space missions to Mars and beyond." },
 ];
